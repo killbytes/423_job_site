b548bd1a250ca933ef4c41527d513b137a8cb623
killbyte <varkillbyte@gmail.com>
2026-09-09 19:36:35 +0300

    feat: clear code

--- src/store/jobs/jobsSelectors.ts
+++ src/store/jobs/jobsSelectors.ts
@@ -4,4 +4,3 @@ export const selectJobsFilters = (state: RootState) => state.jobs;
 export const selectSearch = (state: RootState) => state.jobs.search;
 export const selectCity = (state: RootState) => state.jobs.city;
 export const selectSkills = (state: RootState) => state.jobs.skills;
-export const selectPage = (state: RootState) => state.jobs.page;
